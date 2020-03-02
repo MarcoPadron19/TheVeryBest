fc7ed7952ef5ca89009c2d0c646661cca911698c
MarcoPadron19 <61167378+MarcoPadron19@users.noreply.github.com>
2020-03-02 17:19:14 -0600

    Monday Push
    Assets

---
+++ Marco_Jacob_Porject/Assets/Scripts/Spinnies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinnies : MonoBehaviour
+{
+    public int xspeed;
+    public int yspeed;
+    public int zspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xspeed, yspeed, zspeed, Space.World);
+    }
+}
